cc5358703007712cc96a533981d978c35a192109
zi12i <110373482+zi12i@users.noreply.github.com>
2022-10-03 22:23:38 +0900

    restarted to resolve error

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 Vue.use(VueRouter);
 
@@ -7,6 +8,16 @@ const routes = [
   {
     path: "/",
     name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
 ];
 
